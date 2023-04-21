43525160bfc5355d363c27bf5f1bf368c104a728
giannis <63113599+giannis554@users.noreply.github.com>
2023-04-21 19:28:23 +0300

    Add files via upload

---
+++ Simple-Tic-Tac-Toe/src/Main.java
@@ -0,0 +1,259 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+
+        System.out.println("---------");
+        System.out.println("|" + "       " + "|");
+        System.out.println("|" + "       " + "|");
+        System.out.println("|" + "       " + "|");
+        System.out.println("---------");
+        boolean yourBoolean = true;
+        int counter = 0;
+        boolean yourSecondBoolean = true;
+        while (yourBoolean) {
+            do {
+
+
+                System.out.print("Enter the coordinates: ");
+                String firstAndSecond = scanner.nextLine();
+                String[] parts = firstAndSecond.split("");
+                String first = parts[0];
+                String second = parts[2];
+
+
+                char characterXo = counterXAndO(counter);
+
+                boolean itIsANumber = itIsANumber(first, second);
+                boolean oneToThree = oneToThree(first, second);
+                boolean occupiedVar = occupied(board, first, second, oneToThree);
+
+                if (!itIsANumber) {
+                    System.out.println("You should enter numbers!");
+                } else if (!oneToThree) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                } else if (occupiedVar) {
+                    System.out.println("This cell is occupied! Choose another one!");
+                } else if (!occupiedVar) {
+
+                    int one = Integer.parseInt(first);
+                    int two = Integer.parseInt(second);
+
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (board[i][j] == board[one - 1][two - 1]) {
+                                board[one - 1][two - 1] = characterXo;
+
+                                break;
+                            }
+                        }
+                    }
+
+                    counter++;
+                }
+                if (!itIsANumber || !oneToThree || occupiedVar) {
+                    continue;
+                } else {
+                    System.out.println("---------");
+                    System.out.println("|" + " " + board[0][0] + " " + board[0][1] + " " + board[0][2] + " " + "|");
+                    System.out.println("|" + " " + board[1][0] + " " + board[1][1] + " " + board[1][2] + " " + "|");
+                    System.out.println("|" + " " + board[2][0] + " " + board[2][1] + " " + board[2][2] + " " + "|");
+                    System.out.println("---------");
+                    yourSecondBoolean = false;
+                }
+
+
+            } while (yourSecondBoolean);
+
+
+            boolean oWinsVar = oWins(board);
+            boolean xWinsVar = xWins(board);
+            boolean drawOAndX = draw(board);
+
+
+            if (xWinsVar) {
+                System.out.print("X wins");
+                yourBoolean = false;
+            } else if (oWinsVar) {
+                System.out.print("O wins");
+                yourBoolean = false;
+            } else if (drawOAndX) {
+                System.out.print("Draw");
+                yourBoolean = false;
+            }
+        }
+    }
+
+
+    public static boolean draw(char[][] board) {
+        boolean drawBoolean = false;
+        int counterX = counterForX(board);
+        int counterO = counterForO(board);
+        boolean booleanX = xWins(board);
+        boolean booleanO = oWins(board);
+
+        if (counterO + counterX == 9 && !booleanX && !booleanO) {
+            drawBoolean = true;
+        }
+
+        return drawBoolean;
+    }
+
+    public static int counterForX(char[][] board) {
+        int count = 0;
+        for (char[] chars : board) {
+            for (char aChar : chars)
+                if (aChar == 'X') {
+                    count++;
+                }
+        }
+        return count;
+    }
+
+    public static int counterForO(char[][] board) {
+        int count = 0;
+        for (char[] chars : board) {
+            for (char aChar : chars)
+                if (aChar == 'O') {
+                    count++;
+                }
+        }
+        return count;
+    }
+
+
+    public static boolean xWins(char[][] board) {
+        boolean booleanLogicX = false;
+
+        if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+            booleanLogicX = true;
+        } else if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
+            booleanLogicX = true;
+        } else if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') {
+            booleanLogicX = true;
+        } else if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') {
+            booleanLogicX = true;
+        } else if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') {
+            booleanLogicX = true;
+        } else if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') {
+            booleanLogicX = true;
+        } else if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+            booleanLogicX = true;
+        } else if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+            booleanLogicX = true;
+        }
+        return booleanLogicX;
+    }
+
+    public static boolean oWins(char[][] board) {
+        boolean booleanLogicO = false;
+
+        if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') {
+            booleanLogicO = true;
+        } else if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') {
+            booleanLogicO = true;
+        } else if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') {
+            booleanLogicO = true;
+        } else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') {
+            booleanLogicO = true;
+        } else if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') {
+            booleanLogicO = true;
+        } else if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') {
+            booleanLogicO = true;
+        } else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+            booleanLogicO = true;
+        } else if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+            booleanLogicO = true;
+        }
+        return booleanLogicO;
+    }
+
+
+    public static char counterXAndO(int counter) {
+        char characterXo = '\u0000';
+
+
+        if (counter == 0) {
+            characterXo = 'X';
+        } else if (counter == 1) {
+            characterXo = 'O';
+        } else if (counter == 2) {
+            characterXo = 'X';
+        } else if (counter == 3) {
+            characterXo = 'O';
+        } else if (counter == 4) {
+            characterXo = 'X';
+        } else if (counter == 5) {
+            characterXo = 'O';
+        } else if (counter == 6) {
+            characterXo = 'X';
+        } else if (counter == 7) {
+            characterXo = 'O';
+        } else if (counter == 8) {
+            characterXo = 'X';
+
+        }
+        return characterXo;
+    }
+
+
+    public static boolean itIsANumber(String first, String second) {
+        boolean var = false;
+
+        try {
+
+            if (first.matches("\\d") || second.matches("\\d")) {
+                var = true;
+            }
+        } catch (NumberFormatException e) {
+            var = false;
+        }
+        return var;
+    }
+
+    public static boolean oneToThree(String first1, String second1) {
+        boolean oneToThree = false;
+
+        try {
+            int first = Integer.parseInt(first1);
+            int second = Integer.parseInt(second1);
+            if (first >= 1 && first <= 3 && second >= 1 && second <= 3) {
+                oneToThree = true;
+            }
+        } catch (NumberFormatException e) {
+            oneToThree = false;
+
+        }
+        return oneToThree;
+    }
+
+    public static boolean occupied(char[][] board, String first1, String second1, boolean oneToThree) {
+        boolean occupied = false;
+
+        if (oneToThree) {
+            int first = Integer.parseInt(first1);
+            int second = Integer.parseInt(second1);
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == board[first - 1][second - 1] && (board[i][j] == 'X' || board[i][j] == 'O')) {
+
+                        occupied = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return occupied;
+    }
+}
